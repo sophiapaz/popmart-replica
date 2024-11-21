1054d9f7a208bb5646f1214e8767076b2d040fea
sophiapaz <sophiacp900@gmail.com>
2024-11-20 20:12:34 -0800

    did cart with hannah!

--- home-page-script.js
+++ home-page-script.js
@@ -1,14 +1,15 @@
 //Plan:
 //1. Make the counter of the bag go up when a button is clicked
 //2. make a carousel for the products
-
-
 const counterDisplayElem = document.getElementById('cart-counter');
-let addToBag = document.querySelectorAll('.add-to-bag');
+let addToBag = document.getElementById('add-to-bag');
 let count = 0;
-
 
 addToBag.addEventListener("click",()=>{
     count++;
     counterDisplayElem.textContent = count; //displaying the number
 });
+
+
+
+// trying to do a carousel
