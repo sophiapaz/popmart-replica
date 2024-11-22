438d31b2b61601bc01fb7f7bda37272eedbd79f4
sophiapaz <sophiacp900@gmail.com>
2024-11-21 20:48:08 -0800

    finished carousel for the home page and made edits to product page

--- home-page-script.js
+++ home-page-script.js
@@ -5,11 +5,28 @@ const counterDisplayElem = document.getElementById('cart-counter');
 let addToBag = document.getElementById('add-to-bag');
 let count = 0;
 
+/*
 addToBag.addEventListener("click",()=>{
     count++;
     counterDisplayElem.textContent = count; //displaying the number
 });
-
-
+*/
 
 // trying to do a carousel
+let element1 = document.getElementById('product-team-one');
+let element2 = document.getElementById('product-team-two');
+
+let nextSlide = document.getElementById('right-btn');
+let prevSlide =document.getElementById('left-btn');
+
+nextSlide.addEventListener("click",()=>{
+    console.log("button clicked!");
+    element1.style.display='none';
+    element2.style.display='flex';
+});
+
+prevSlide.addEventListener("click",()=>{
+    console.log("button clicked!");
+    element2.style.display='none';
+    element1.style.display='flex';
+});
